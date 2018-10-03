c99ccc656ac37860d3c5fbc346ce59ed0f05a085
Thassya <thassya@ice.ufjf.br>
2018-10-02 22:19:04 -0300

    1st

--- script.sh
+++ script.sh
@@ -5,3 +5,4 @@ docker exec -it gcc_compiler sh -c "chmod 755 /home/compiler/input/timeout_scrip
 docker exec -it gcc_compiler sh -c "if gcc /home/compiler/input/input.c -o /home/compiler/input/input_compiled; then echo ""Compile Successful""> /home/compiler/output/status.txt; else echo ""Compile Error"" > /home/compiler/output/status.txt; fi " 
 docker exec -it gcc_compiler sh -c "./home/compiler/input/timeout_script.sh"
 docker stop gcc_compiler  && docker cp gcc_compiler:/home/compiler/output/output.txt /home/thassya/Documentos/Docker/ && docker cp gcc_compiler:/home/compiler/output/status.txt /home/thassya/Documentos/Docker/ 
+ 
